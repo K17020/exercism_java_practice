e0dd7d474b4ddee7afbaef1166924b5e533e04c0
Kevin Tam <tam.kevin@hotmail.com>
2017-09-28 16:47:59 -0700

    finish test 1

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,5 +1,9 @@
 public class Twofer {
     public String twofer(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // User Enter Name
+        // Based off string return a specific string
+        if(name == null) {
+        }
+        return "One for you, one for me.";
     }
 }
